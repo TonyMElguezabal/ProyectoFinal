3bd6ec1f400cc1ef91e0d224bf43c2b05118c732
Tony MElguezabal <jose.elguezabal@gmail.com>
2020-06-06 10:27:10 -0500

    Login page ya jala en version basica hardodeada de datos

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,24 @@
-//comentario para el JS
 import css from "./estilos.css";
+const usuarios = [
+  {
+    user: "Antonio",
+    password: "1234",
+  },
+];
 
-alert("Hola mundo del JS");
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const usuario = loginForm.username.value;
+  const pass = loginForm.password.value;
+
+  if (usuario === "user" && pass === "web_dev") {
+    alert("Entraste al sistema");
+    console.log("Ya entro al sistema");
+  } else {
+    alert("Error de login");
+  }
+});
